f2d576f3df73f9eab1671c68200ed88f749462ed
BellidoJeremy <jeremy.bellido@tecsup.edu.pe>
2024-03-24 14:04:35 -0500

    First commit

---
+++ src/main/java/con/tecsup/lab03/lab03_4C24_GBA8/App.java
@@ -0,0 +1,13 @@
+package con.tecsup.lab03.lab03_4C24_GBA8;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
